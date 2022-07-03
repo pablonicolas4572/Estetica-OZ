1afeb8f090e7f5cc98fcd8cc0b141aeab9a73aac
pablo nicolas <pabloquinteros1413@gmail.com>
2022-07-02 22:49:23 -0300

    javascript

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 //Descuento del 30% por la compra de 2 servicios o más.
-const servicios = prompt("ingrese la cantidad de servicios que quiere");
 
-const clase = prompt("¿Qué clase de servicio buscás?")
+function welcomePer(nombres, apellidos) {
+    alert (`Bienvenido/a ${nombres} ${apellidos}`);
+}
 
+let nombres = prompt("Ingrese sus nombres")
+let apellidos = prompt("Ingrese sus apellido")
+
+welcomePer(nombres, apellidos);
+
+let servicios = prompt('ingrese la cantidad de servicios que quiere');
+let clase = prompt("¿Qué clase de servicio buscás?")
 
 if (servicios >= 2 && clase == "Baño lunar"){
     alert ("Recibirás un descuento.")
@@ -10,6 +18,7 @@ if (servicios >= 2 && clase == "Baño lunar"){
 else if (servicios >= 2 && clase == "Masaje descontracturante"){
     alert ("Recibirás un descuento.")
 }
-else (servicios <=1); {
+else (servicios < 2); {
     alert ("Recordá que si contratas 2 o más de nuestros servicios seleccionados recibirás un descuento del 30%.")
 }
+
